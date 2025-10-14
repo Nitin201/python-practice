64533f711e6268ba9577ebfb183920206e54d393
nitin <dodamanin8@gmail.com>
2025-10-14 12:39:23 +0530

    list

---
+++ list.py
@@ -0,0 +1,20 @@
+numbers = [10,20,30,40,50]
+
+print(numbers)
+
+for x in numbers:
+    print(x)
+
+if 100 in numbers:
+    print("yes") 
+else:
+    print("no")
+
+
+ # list index
+ 
+print("last elelment", numbers[-1])
+
+numbers[2] = 100
+print(numbers)
+
